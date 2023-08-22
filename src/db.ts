e7422a651842b6b82a0fd4b00710ab564ac4724c
simonflash1 <nestor.emmanuel.lizarraga@gmail.com>
2023-08-22 18:57:30 -0300

    database Sync

--- src/db.ts
+++ src/db.ts
@@ -6,7 +6,14 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/Tayta`);
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/Tayta`, {
+  dialect: 'postgres', // El dialecto de tu base de datos
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Esto puede variar según tu base de datos
+    }
+  }
+});
 
 export default sequelize;
