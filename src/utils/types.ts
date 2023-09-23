24fd2adf8adfa9df9fa03eee80dab0738a937763
simonflash1 <nestor.emmanuel.lizarraga@gmail.com>
2023-09-23 18:20:35 -0300

    types actualizados

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -8,15 +8,32 @@ export interface IProduct {
   precioPorUnidad: number;
 }
 
+export enum rolUsuario {
+  ADMIN = 'admin',
+  DEPARTAMENTO = 'departamento',
+  MUNICIPALIDAD = 'municipalidad',
+  PRODUCTOR = 'productor',
+}
+
 export interface IUser {
-  id?: number;
-  rol: 'admin' | 'municipalidad';
-  username: string;
-  password: string;
-  nombreMunicipio: string;
-  departamento: string;
-  provincia: string;
-  distrito: string;
+  id?: string;
+  rol: rolUsuario;
+  idPais: number;
+  idDepartamento?: number;
+  idMunicipalidad?: number;
+  email?: string;
+
+  nombreEntidad?: string;
+
+  username?: string;
+  password?: string;
+
+  nombres?: string;
+  apellidoPaterno?: string;
+  apellidoMaterno?: string;
+  dni?: string;
+  domicilio?: string;
+  telefono?: string;
 }
 
 export interface ISeller {
@@ -28,4 +45,21 @@ export interface ISeller {
   domicilio: string;
   telefono: string;
   municipioId: number;
+}
+
+export interface Negocio {
+  id?: number;
+  idProductor: number;
+  nombreNegocio: string;
+  categoria: string;
+}
+
+export interface IProduct {
+  id: number;
+  nombre: string;
+  categoria: string;
+  descripcion: string;
+  imagen: string;
+  unidadMedida: 'unidad' | 'kg' | 'litro';
+  precioPorUnidad: number;
 }
