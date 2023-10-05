a348069d3fc7b601a4b6e2fc89f023ddc17091c6
simonflash1 <nestor.emmanuel.lizarraga@gmail.com>
2023-10-04 21:20:41 -0300

    Negocio agregado

--- src/db.ts
+++ src/db.ts
@@ -8,6 +8,12 @@ const {
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/Tayta`, {
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 export default sequelize;
